538506edcf9a3431fb22a4551567e4023f78f675
cam <cammclain@skiff.com>
2024-07-12 20:29:00 -0500

    add start function to application

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import logging
 
 from telegram import Update
 from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
-
+from cryptbot.commands.base_commands.start import start
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
@@ -14,3 +14,15 @@ logging.getLogger(__name__)
 
 
 # import commands
+
+
+
+
+def main() -> None:
+    application = Application.builder().token("TOKEN").build()
+    
+    #### COMMANDS HERE ####
+
+    application.add_handler(CommandHandler("start", start))
+
+    application.run_polling()
