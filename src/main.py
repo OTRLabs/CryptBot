7a548c648304fab139d8da663033831aec754f21
cam <cammclain@skiff.com>
2024-07-12 20:13:11 -0500

    setup logging

--- src/main.py
+++ src/main.py
@@ -2,3 +2,15 @@ import logging
 
 from telegram import Update
 from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger(__name__)
+
+
+
+# import commands
