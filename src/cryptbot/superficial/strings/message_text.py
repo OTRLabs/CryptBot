ddba8987c59874ac9f71ad434a039a2ed6855ef7
Cam <cammclain@skiff.com>
2024-07-15 17:30:01 +0000

    updating messages to use a class + set type def

--- src/cryptbot/superficial/strings/message_text.py
+++ src/cryptbot/superficial/strings/message_text.py
@@ -1,6 +1,6 @@
 
 
+class Messages:
+    START_MESSAGE: str = "Hello, I am CryptBot!\n\nI am a Telegram Bot designed to assist you with various offensive cyber security activites!\n\nUse /help for a list of commandsto get started!"
 
-START_MESSAGE = "Hello, I am CryptBot!\n\nI am a Telegram Bot designed to assist you with various offensive cyber security activites!\n\nUse /help for a list of commandsto get started!"
-
-HELP_MESSAGE = "Command List:\n\n\n/start - Start the bot\n/help - Get help with the bot"
+    HELP_MESSAGE: str = "Command List:\n\n\n/start - Start the bot\n/help - Get help with the bot"
