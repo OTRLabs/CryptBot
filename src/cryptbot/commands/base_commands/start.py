b2df4fa202704660dd445b580fd630f4f04d6e02
Cam <cammclain@skiff.com>
2024-07-15 17:44:01 +0000

    adding commands

--- src/cryptbot/commands/base_commands/start.py
+++ src/cryptbot/commands/base_commands/start.py
@@ -1,7 +1,9 @@
 import logging
 
-from telegram import ForceReply, Update
+from telegram import ForceReply, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
+from ...superficial.strings.message_text import Messages
+
 
 
 
@@ -9,7 +11,40 @@ from telegram.ext import ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
+    
+    start_buttons = [
+        [
+            InlineKeyboardButton(
+                "🤖 Help", callback_data="help"
+            ),
+            InlineKeyboardButton(
+                "📚 Commands", callback_data="commands"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                "Services", callback_data="services"
+            ),
+            InlineKeyboardButton(
+                "Projects", callback_data="about"
+            ),
+            InlineKeyboardButton(
+                "Reports", callback_data="reports"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "🔐 About", callback_data="about"
+            ),
+            InlineKeyboardButton(
+                "Settings", callback_data="settings"
+            )
+    ]
+]
+    start_keyboard = InlineKeyboardMarkup(start_buttons)
+    
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
+        Messages.START_MESSAGE,
         reply_markup=ForceReply(selective=True),
+        reply_markup=start_keyboard
     )
